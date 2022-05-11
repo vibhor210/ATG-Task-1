5d44ced222b9379da01f33f928d0eb237235a021
vibhor210 <vermavibhor21@gmail.com>
2022-05-11 10:43:36 +0530

    main

--- authentication/settings.py
+++ authentication/settings.py
@@ -80,28 +80,14 @@ WSGI_APPLICATION = 'authentication.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 # Default
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
-DATABASES = {  
-    'default': {  
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': os.environ['DATABASE_NAME'],  
-        'USER': os.environ['DATABASE_USER'],  
-        'PASSWORD': os.environ['DATABASE_PASSWORD'],  
-        'HOST': '127.0.0.1',  
-        'PORT': '3306',  
-        'OPTIONS': {  
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-        }  
-    }  
-}  
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
